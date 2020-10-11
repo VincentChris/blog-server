37ee7fa438469cf26b3627f234928316a91dd9ee
VincentChris <vincentchris@126.com>
2020-10-11 14:55:38 +0800

    路由模块拆分

--- app/router.ts
+++ app/router.ts
@@ -1,8 +1,9 @@
 import { Application } from 'egg';
+import defaultRoute from './router/default';
 
 export default (app: Application) => {
   const { controller, router } = app;
-
+  defaultRoute(app);
   router.get('/', controller.home.index);
   router.get('/list', controller.home.list);
 };
